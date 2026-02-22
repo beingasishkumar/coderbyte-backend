0a73ba633b6b4ddff60369efd13b5b53b01503b3
asish <beingasishkumar@gmail.com>
2026-02-22 21:14:57 +0530

    init

--- src/main/java/com/coderbyte/test/controller/TaskController.java
+++ src/main/java/com/coderbyte/test/controller/TaskController.java
@@ -9,33 +9,34 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api/tasks")
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
-    @PostMapping("/save")
+    @PostMapping
     public ResponseEntity<Task> save(@Valid @RequestBody Task task) {
         return ResponseEntity.ok(taskService.save(task));
     }
 
-    @GetMapping("/getAll")
+    @GetMapping
     public ResponseEntity<List<Task>> getAll() {
         return ResponseEntity.ok(taskService.getAll());
     }
 
-    @GetMapping("/get/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Task> get(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.get(id));
     }
 
-    @PutMapping("/update")
+    @PutMapping
     public ResponseEntity<Task> update(@Valid @RequestBody Task task) {
         return ResponseEntity.ok(taskService.update(task));
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         taskService.delete(id);
         return ResponseEntity.noContent().build();
